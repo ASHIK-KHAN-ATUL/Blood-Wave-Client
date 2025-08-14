257574e81a9a21e3e1a4c88fc7a5be64bcac865c
ASHIK-KHAN-ATUL <ashikkhan693693@gmail.com>
2025-08-14 23:16:53 +0600

    some modification Done 2

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-    // baseURL: 'https://blood-wave-server-six.vercel.app'
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://blood-wave-server-six.vercel.app'
+    // baseURL: 'http://localhost:5000'
 })
 
 const useAxiosSecure = () => {
