53f1be85a3a0941b7f7cd403dbe6a9fbfe890390
ASHIK-KHAN-ATUL <ashikkhan693693@gmail.com>
2025-09-16 23:59:23 +0600

    new stats and many

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosPublic = axios.create({
-  // baseURL: "https://blood-wave-server-six.vercel.app",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://blood-wave-server-six.vercel.app",
+  // baseURL: "http://localhost:5000",
 });
 
 const useAxiosPublic = () => {
